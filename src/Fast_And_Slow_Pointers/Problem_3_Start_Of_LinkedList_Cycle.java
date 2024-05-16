a885803e79dbb02331363d4dd5054b697527383b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-05-16 10:59:10 +0100

    Ninth Commit: Add "Start of LinkedList Cycle" problem to "Fast and Slow" pattern section

---
+++ src/Fast_And_Slow_Pointers/Problem_3_Start_Of_LinkedList_Cycle.java
@@ -0,0 +1,36 @@
+package Fast_And_Slow_Pointers;
+
+// Design Guru Problem Statement: Start of LinkedList Cycle
+// LeetCode Question: 142. Linked List Cycle II
+
+public class Problem_3_Start_Of_LinkedList_Cycle {
+    class ListNode{
+        int val = 0;
+        ListNode next;
+
+        public ListNode(int value){
+            this.val = value;
+        }
+    }
+
+    public ListNode findCycleStart(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == head) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while(slow != fast){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
