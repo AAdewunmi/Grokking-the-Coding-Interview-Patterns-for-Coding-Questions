a30b1110843edac617d9bf2d786d4f097e56cf83
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-05-28 09:55:26 +0100

    Fifty Commit: Add Reverse a LinkedList problem to In Place Reversal Linked List Pattern section

---
+++ src/In_Place_Reversal_LinkedList_Pattern/Problem_1_ReverseALinkedList.java
@@ -0,0 +1,27 @@
+package In_Place_Reversal_LinkedList_Pattern;
+
+// Problem Statement: Reverse a LinkedList (easy)
+// LeetCode Question: 206. Reverse Linked List
+
+public class Problem_1_ReverseALinkedList {
+    class ListNode {
+        int val = 0;
+        ListNode next;
+        ListNode(int value){
+            this.val = value;
+        }
+    }
+
+    public ListNode reverse (ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
