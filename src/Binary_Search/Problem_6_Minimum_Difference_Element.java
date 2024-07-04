2c794e653898a4b14a73eed58017f80b668779cb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 16:12:29 +0100

    One-Hundred-Forty-Three Commit: Add Minimum Difference Element problem to Binary Search section

---
+++ src/Binary_Search/Problem_6_Minimum_Difference_Element.java
@@ -0,0 +1,29 @@
+package Binary_Search;
+
+// Problem Statement: Minimum Difference Element (medium)
+// LeetCode Question: 1200. Minimum Absolute Difference
+
+public class Problem_6_Minimum_Difference_Element {
+    public static int searchMinDiffElement(int[] arr, int key){
+        if (key < arr[0]) {
+            return arr[0];
+        }
+        if (key > arr[arr.length - 1]) {
+            return arr[arr.length - 1];
+        }
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]){
+                start = mid + 1;
+            } else {
+                return arr[mid];
+            }
+        }
+        if ((arr[start] - key) < (key - arr[end]))
+            return arr[start];
+        return arr[end];
+    }
+}
