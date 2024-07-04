289c6fb63baf7bf59791cefbcf80a0a09ae9581d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 10:13:00 +0100

    One-Hundred-Thirty-Eight Commit: Add Binary Search problem to Binary Search section

---
+++ src/Binary_Search/Problem_1_Binary_Search.java
@@ -0,0 +1,31 @@
+package Binary_Search;
+
+// Problem Statement: Order-agnostic Binary Search (easy)
+// LeetCode Question: 704. Binary Search
+
+public class Problem_1_Binary_Search {
+
+    public static int search(int[] arr, int key){
+        int start = 0, end = arr.length - 1;
+        boolean isAscending = arr[start] < arr[end];
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key == arr[mid]) return mid;
+            if (isAscending) {
+                if (key < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (key > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
