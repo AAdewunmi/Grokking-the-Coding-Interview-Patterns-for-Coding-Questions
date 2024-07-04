94583bf86d9dbe664d2dfae75e67256126535502
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 10:43:20 +0100

    One-Hundred-Thirty-Nine Commit: Add Ceiling of a Number problem to Binary Search section

---
+++ src/Binary_Search/Problem_2_Ceiling_Of_A_Problem.java
@@ -0,0 +1,22 @@
+package Binary_Search;
+
+// Problem Statement: Ceiling of a Number
+// LeetCode Question:
+
+public class Problem_2_Ceiling_Of_A_Problem {
+    public static int searchCeilingOfANumber(int[] arr, int key){
+        if (key > arr[arr.length - 1]) return -1;
+        int start = 0, end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]){
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
