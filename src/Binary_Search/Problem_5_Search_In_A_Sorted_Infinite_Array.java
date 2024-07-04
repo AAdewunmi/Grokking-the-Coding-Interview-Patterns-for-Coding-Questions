f06f2931eed6e3572d66d7e7b29083357bc237d9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 15:27:09 +0100

    One-Hundred-Forty-Two Commit: Add Search in a Sorted Infinite Array problem to Binary Search section

---
+++ src/Binary_Search/Problem_5_Search_In_A_Sorted_Infinite_Array.java
@@ -0,0 +1,44 @@
+package Binary_Search;
+
+// Problem Statement: Search in a Sorted Infinite Array (medium)
+// LeetCode Question: 702. Search in a Sorted Array of Unknown Size
+
+public class Problem_5_Search_In_A_Sorted_Infinite_Array {
+
+    class ArrayReader {
+        int[] arr;
+        ArrayReader(int[] arr) {
+            this.arr = arr;
+        }
+        public int get(int index) {
+            if (index >= arr.length)
+                return Integer.MAX_VALUE;
+                return arr[index];
+        }
+    }
+
+    public static int searchInfiniteSortedArray(ArrayReader reader, int key){
+        int start = 0, end = 1;
+        while (reader.get(end) < key) {
+            int newStart = end + 1;
+            end += (end - start + 1) * 2;
+            start = newStart;
+        }
+        return binarySearch(reader, key, start, end);
+    }
+
+    private static int binarySearch (ArrayReader reader, int key, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < reader.get(mid)) {
+                end = mid - 1;
+            } else if (key > reader.get(mid)) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return - 1;
+    }
+
+}
