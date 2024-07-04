6314fc738c1dee409763d36bca4379adc4d20808
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 12:59:59 +0100

    One-Hundred-Forty-One Commit: Add Number Range problem to Binary Search section

---
+++ src/Binary_Search/Problem_4_Number_Range.java
@@ -0,0 +1,35 @@
+package Binary_Search;
+
+// Problem Statement: Number Range (medium)
+// LeetCode Question: 34. Find First and Last Position of Element in Sorted Array
+
+public class Problem_4_Number_Range {
+    public int[] findRange(int[] arr, int key){
+        int[] result = new int[] {-1, -1};
+        result[0] = search(arr, key, false);
+        if (result[0] != -1) result[1] = search(arr, key, true);
+        return result;
+    }
+
+    // Modified Binary Search
+    private static int search (int[] arr, int key, boolean findMaxIndex) {
+        int keyIndex = -1;
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                keyIndex = mid;
+                if (findMaxIndex) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return keyIndex;
+    }
+}
