eaeab500ff938831a336c53b93bdec09cc73dd99
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 16:18:56 +0100

    One-Hundred-Forty-Four Commit: Add Bitonic Array Maximum problem to Binary Search section

---
+++ src/Binary_Search/Problem_7_Bitonic_Array.java
@@ -0,0 +1,19 @@
+package Binary_Search;
+
+// Problem Statement: Bitonic Array Maximum (easy)
+// LeetCode Question:
+
+public class Problem_7_Bitonic_Array {
+    public int findMax(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return arr[start];
+    }
+}
