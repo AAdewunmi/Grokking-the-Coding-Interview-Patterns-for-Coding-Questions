e67c862b2bb74ce7054c5736ced2f72b68be80db
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-04 11:05:59 +0100

    One-Hundred-Forty Commit: Add Next Letter to Binary Search section

---
+++ src/Binary_Search/Problem_3_Next_Letter.java
@@ -0,0 +1,21 @@
+package Binary_Search;
+
+// Problem Statement: Next Letter (medium)
+// LeetCode Question: 744. Find Smallest Letter Greater Than Target
+
+
+public class Problem_3_Next_Letter {
+    public char searchNextLetter(char[] letters, char key){
+        int n = letters.length;
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % n];
+    }
+}
