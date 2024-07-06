e84c83f62fbaa83fa5a20db86722cb8ed0a312fc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-06 12:12:58 +0100

    One-Hundred-Sixty Commit: Add  Top 'K' Numbers problem to Top K Elements section

---
+++ src/Top_K_Elements/Problem_2_Kth_Smallest_Number.java
@@ -0,0 +1,24 @@
+package Top_K_Elements;
+
+// Problem Statement: Top 'K' Numbers (easy)
+// LeetCode Question:
+
+import java.util.PriorityQueue;
+
+public class Problem_2_Kth_Smallest_Number {
+    public int findKthSmallestNumber(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((n1, n2) -> n2 - n1);
+
+        for (int i = 0; i < k; i++)
+            maxHeap.add(nums[i]);
+
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] < maxHeap.peek()) {
+                maxHeap.poll();
+                maxHeap.add(nums[i]);
+            }
+        }
+
+        return maxHeap.peek();
+    }
+}
