337150f8866fa368742d0c2e7160af823d45d040
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-09 11:27:09 +0100

    One-Hundred-Eighty-Six Commit: Add Valid Palindrome II problem to Greedy Algorithm section

---
+++ src/Greedy_Algorithm/Problem_1_Valid_Palindrome_11.java
@@ -0,0 +1,29 @@
+package Greedy_Algorithm;
+
+// Problem Statement: Valid Palindrome II
+// LeetCode Question: 680. Valid Palindrome II
+
+public class Problem_1_Valid_Palindrome_11 {
+    public boolean isPalindromePossible(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    // Helper method to check if a substring is a palindrome
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
