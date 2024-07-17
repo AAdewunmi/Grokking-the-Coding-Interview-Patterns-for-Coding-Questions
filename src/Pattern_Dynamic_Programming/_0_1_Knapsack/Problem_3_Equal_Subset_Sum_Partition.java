40da0a19f73715cbafa17266f82cf1c0e517d4f3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-17 15:25:04 +0100

    Two-Hundred-Fifty-One Commit: Add "" problem to Prefix Sum Section

---
+++ src/Pattern_Dynamic_Programming/_0_1_Knapsack/Problem_3_Equal_Subset_Sum_Partition.java
@@ -0,0 +1,34 @@
+package Pattern_Dynamic_Programming._0_1_Knapsack;
+
+// Problem Statement: Subset Sum (medium)
+// LeetCode Question:
+
+public class Problem_3_Equal_Subset_Sum_Partition {
+    public boolean canPartition(int[] num, int sum) {
+        int n = num.length;
+        boolean[] dp = new boolean[sum + 1];
+
+        // handle sum=0, as we can always have '0' sum with an empty set
+        dp[0] = true;
+
+        // with only one number, we can have a subset only when the required sum is equal to
+        // its value
+        for (int s = 1; s <= sum; s++) {
+            dp[s] = (num[0] == s ? true : false);
+        }
+
+        // process all subsets for all sums
+        for (int i = 1; i < n; i++) {
+            for (int s = sum; s >= 0; s--) {
+                // if dp[s]==true, this means we can get the sum 's' without num[i], hence we
+                // can move on to the next number else we can include num[i] and see if we can
+                // find a subset to get the remaining sum
+                if (!dp[s] && s >= num[i]) {
+                    dp[s] = dp[s - num[i]];
+                }
+            }
+        }
+
+        return dp[sum];
+    }
+}
---
+++ src/Pattern_Dynamic_Programming/_0_1_Knapsack/Problem_6_Target_Sum.java
@@ -0,0 +1,33 @@
+package Pattern_Dynamic_Programming._0_1_Knapsack;
+
+// Problem Statement: Target Sum (hard)
+// LeetCode Question: 494. Target Sum
+
+public class Problem_6_Target_Sum {
+
+    public int findTargetSubsets(int[] num, int s) {
+        int totalSum = 0;
+        for (int n : num)
+            totalSum += n;
+        if(totalSum < s || (s + totalSum) % 2 == 1)
+            return 0;
+        return countSubsets(num, (s + totalSum) / 2);
+    }
+
+    private int countSubsets (int[] num, int sum) {
+        int n = num.length;
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+        for(int s=1; s <= sum ; s++) {
+            dp[s] = (num[0] == s ? 1 : 0);
+        }
+        for(int i=1; i < num.length; i++) {
+            for(int s=sum; s >= 0; s--) {
+                if(s >= num[i])
+                    dp[s] += dp[s-num[i]];
+            }
+        }
+        return dp[sum];
+    }
+
+}
