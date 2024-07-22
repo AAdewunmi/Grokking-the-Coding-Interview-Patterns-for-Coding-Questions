f6aea93277d847ead0245b967f7bc1cdf4ba527e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-07-22 13:12:35 +0100

    Two-Hundred-Seventy-Nine Commit: Add Staircase problem to Fibonacci Numbers section

--- src/Dynamic_Programming/Fibonacci_Numbers/Fibonacci_Numbers.java
+++ src/Dynamic_Programming/Fibonacci_Numbers/Fibonacci_Numbers.java
@@ -4,6 +4,41 @@ package Dynamic_Programming.Fibonacci_Numbers;
 // LeetCode Question: 509. Fibonacci Number
 
 public class Fibonacci_Numbers {
+    // Brute Force Approach
+    public int calculateFibonacci_1(int n) {
+        if(n < 2)
+            return n;
+        return calculateFibonacci(n-1) + calculateFibonacci(n-2);
+    }
+
+    // Top-down Dynamic Programming with Memoization Approach
+    public int calculateFibonacci_2(int n) {
+        int dp[] = new int[n + 1];
+        return calculateFibonacciRecursive(dp, n);
+    }
+
+    public int calculateFibonacciRecursive(int[] dp, int n) {
+        if (n < 2)
+            return n;
+        if (dp[n] == 0)
+            dp[n] = calculateFibonacciRecursive(dp, n - 1) + calculateFibonacciRecursive(dp, n - 2);
+        return dp[n];
+    }
+
+    // Bottom-up Dynamic Programming Approach
+    public int calculateFibonacci_3(int n) {
+        if (n < 2)
+            return n;
+
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+
+    // Memory optimization Approach
     public int calculateFibonacci(int n) {
         if (n < 2)
             return n;
---
+++ src/Dynamic_Programming/Fibonacci_Numbers/Staircase.java
@@ -0,0 +1,88 @@
+package Dynamic_Programming.Fibonacci_Numbers;
+
+// Problem Statement: Staircase
+// LeetCode Question: 70. Climbing Stairs
+
+public class Staircase {
+
+    // Brute Force Approach
+    public int countWays(int n) {
+        if( n == 0)
+            return 1; // base case, we don't need to take any step, so there is only one way
+
+        if(n == 1)
+            return 1; // we can take one step to reach the end, and that is the only way
+
+        if(n == 2)
+            return 2; // we can take one step twice or jump two steps to reach at the top
+
+        // if we take 1 step, we are left with 'n-1' steps;
+        int take1Step = countWays(n-1);
+        // similarly, if we took 2 steps, we are left with 'n-2' steps;
+        int take2Step = countWays(n-2);
+        // if we took 3 steps, we are left with 'n-3' steps;
+        int take3Step = countWays(n-3);
+
+        return take1Step + take2Step + take3Step;
+    }
+
+    // Top-down Dynamic Programming with Memoization Approach
+    public int countWays_1(int n) {
+        int dp[] = new int[n+1];
+        return countWaysRecursive(dp, n);
+    }
+
+    public int countWaysRecursive(int[] dp, int n) {
+        if( n == 0)
+            return 1; // base case, we don't need to take any step, so there is only one way
+
+        if(n == 1)
+            return 1; // we can take one step to reach the end, and that is the only way
+
+        if(n == 2)
+            return 2; // we can take one step twice or jump two steps to reach at the top
+
+        if(dp[n] == 0) {
+            // if we take 1 step, we are left with 'n-1' steps;
+            int take1Step = countWaysRecursive(dp, n-1);
+            // similarly, if we took 2 steps, we are left with 'n-2' steps;
+            int take2Step = countWaysRecursive(dp, n-2);
+            // if we took 3 steps, we are left with 'n-3' steps;
+            int take3Step = countWaysRecursive(dp, n-3);
+            dp[n] = take1Step + take2Step + take3Step;
+        }
+
+        return dp[n];
+    }
+
+    // Bottom-up Dynamic Programming Approach
+    public int countWays_2(int n) {
+        if (n < 2) return 1;
+        if (n == 2) return 2;
+
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+
+        return dp[n];
+    }
+
+    // Memory optimization Approach
+    public int countWays_3(int n) {
+        if (n < 2) return 1;
+        if (n == 2) return 2;
+
+        int n1=1, n2=1, n3=2, temp;
+        for(int i=3; i<=n; i++) {
+            temp = n1 + n2 + n3;
+            n1 = n2;
+            n2 = n3;
+            n3 = temp;
+        }
+        return n3;
+    }
+}
